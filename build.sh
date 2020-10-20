4975b0322a4d013b1221487af4357af7c2bbab58
Johan Vester <johan.vester@gmail.com>
2020-10-21 00:01:10 +0200

    Only show successful build messages when compilation exits with code 0

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@
 echo ""
 
 if [ "$1" == "debug" ]; then
-    env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Debug=true" -o bin/sammy-debug.exe cmd/sammy.go
-    echo "Built bin/sammy-debug.exe"
+    env GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Debug=true" -o bin/sammy-debug.exe cmd/sammy.go && echo "Built bin/sammy-debug.exe"
 else
-    env GOOS=windows GOARCH=amd64 go build -ldflags -H=windowsgui -o bin/sammy.exe cmd/sammy.go
-    echo "Built bin/sammy.exe"
+    env GOOS=windows GOARCH=amd64 go build -ldflags -H=windowsgui -o bin/sammy.exe cmd/sammy.go && echo "Built bin/sammy.exe"
 fi
 
